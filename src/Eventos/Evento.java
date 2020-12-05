8ccc9e621da9a8dbbbc51dd91c3e73febe23f3ea
OnionService <74696654+OnionService@users.noreply.github.com>
2020-12-04 21:46:28 -0500

    Update Evento.java

--- src/Eventos/Evento.java
+++ src/Eventos/Evento.java
@@ -1,35 +1,58 @@
 package Eventos;
 
-import Usuarios.Cliente;
-import Usuarios.Planificador;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.Iterator;
+import java.util.ArrayList;
 
+/**
+ *
+ * @author alber
+ */
 public class Evento {
-
     private TipoEvento tipo;
-    private double tarifa;
     private String sTipo;
+    private double tarifa;
     private Date fecha;
     private String codigo;
-    private EstadoEvento estado = EstadoEvento.PENDIENTE;
+    private EstadoEvento estado=EstadoEvento.PENDIENTE;
     private Cliente cl;
     private Date hInicio;
     private Date hFin;
     private int capacidad;
     private Planificador pl;
-    private char desicion;
     private Comida comida;
     private Bebida bebida;
     private Musica musica;
     private Fotografia fotografia;
     private Bocaditos bocaditos;
+    private char desicion;
     private static ArrayList<Evento> eventos = new ArrayList<>();
-
-    public Evento(TipoEvento tipo, Date fecha, String codigo, Cliente cl, Date hInicio, Date hFin, int capacidad, char desicion,Planificador pl) {
+    
+    public Evento(TipoEvento tipo, Date fecha, Cliente cl,Date hInicio, Date hFin){
+        this.tipo=tipo;
+        this.defTarifa();
+        this.fecha=fecha;
+        this.cl=cl;
+        this.tipoToString();
+        this.hInicio=hInicio;
+        this.hFin=hFin;
+        
+    }
+    
+    //constructor con valores por defecto para crear evento cuando se crea la solicitud 
+    public Evento(TipoEvento tipo, Date fecha, Cliente cl){
+        this.tipo=tipo;
+        this.defTarifa();
+        this.fecha=fecha;
+        this.cl=cl;
+        this.tipoToString();
+        this.hInicio=null;
+        this.hFin=null;
+    }
+    
+     public Evento(TipoEvento tipo, Date fecha, String codigo, Cliente cl, Date hInicio, Date hFin, int capacidad, char desicion,Planificador pl) {
         this.tipo = tipo;
         this.fecha = fecha;
         this.codigo = codigo;
@@ -40,76 +63,71 @@ public class Evento {
         this.desicion = desicion;
         this.pl = pl;
     }
-    
-    
-
     public static ArrayList<Evento> getEventos() {
         return eventos;
     }
-
-    public static void setEventos(ArrayList<Evento> eventos) {
-        Evento.eventos = eventos;
+    public void setTipo(TipoEvento tipo){
+        this.tipo=tipo;
     }
-
-   public double getTarifa(){
-        return tarifa;
-    }
-
     
-    public void setTipo(TipoEvento tipo) {
-        this.tipo = tipo;
-    }
-
-    public TipoEvento getTipo() {
+    public TipoEvento getTipo(){
         return tipo;
     }
-
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
+      
+    public double getTarifa(){
+        return tarifa;
     }
-
-    public Date getFecha() {
+    
+    public void setFecha(Date fecha){
+        this.fecha=fecha;
+    }
+    
+    public Date getFecha(){
         return fecha;
     }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
+    
+    public void setCodigo(String codigo){
+        this.codigo=codigo;
     }
-
-    public String getCodigo() {
+    
+    public String getCodigo(){
         return codigo;
     }
-
-    public void setCapacidad(int capacidad) {
-        this.capacidad = capacidad;
+    
+    public void setCapacidad(int capacidad){
+        this.capacidad=capacidad;
     }
-
-    public int getCapacidad() {
+    
+    public int getCapacidad(){
         return capacidad;
     }
-
-    public void setPlanificador(Planificador pl) {
-        this.pl = pl;
+    
+    public void setPlanificador(Planificador pl){
+        this.pl=pl;
     }
-
-    public Planificador getPlanificador() {
+    
+    public Planificador getPlanificador(){
         return pl;
     }
-
-    public void setInicio(Date hInicio) {
-        this.hInicio = hInicio;
+    
+    public void setInicio(Date hInicio){
+        this.hInicio=hInicio;
     }
-
-    public Date getInicio() {
+    
+    public Date getInicio(){
         return hInicio;
     }
-
-    public void setFin(Date hFin) {
-        this.hFin = hFin;
+    
+    public void setFin(Date hFin){
+        this.hFin=hFin;
     }
-
-    public Date getFin() {
+    
+    public Date getFin(){
         return hFin;
+    }
+    
+    public Cliente getCliente(){
+        return cl;
     }
     
     public void setComida(Comida comida){
@@ -127,6 +145,44 @@ public class Evento {
     public void setBocaditos(Bocaditos bocaditos){
         this.bocaditos=bocaditos;
     }
+    public Comida getComida(){
+        return comida;
+    }
+    public Bebida getBebida(){
+        return bebida;
+    }
+    public Musica getMusica(){
+        return musica;
+    }
+    public Fotografia getFotografia(){
+        return fotografia;
+    }
+    public Bocaditos getBocaditos(){
+        return bocaditos;
+    }
+    
+    
+    static ArrayList<String> codigos=new ArrayList<>();
+    public static String generarCodigo(){
+        
+        String newCode="";
+        String codigo="";
+        while (!codigos.contains(codigo) && codigo.equals("")){
+            Random rd = new Random();
+            IntStream code=rd.ints(4,0,10);
+            Iterator iterator =code.iterator();
+            while(iterator.hasNext()){
+                newCode=iterator.next()+"";
+                codigo=codigo+newCode;            
+            }
+        }
+        codigos.add(codigo);
+        return codigo;
+    }
+    
+    public void confirmarEvento(){
+        this.estado=EstadoEvento.CONFIRMADO;
+    }
     
     public void defTarifa(){
        switch(tipo){
@@ -141,28 +197,7 @@ public class Evento {
         }
         
     }
-    public static String generarCodigo() {
-        ArrayList<String> codigos = new ArrayList<>();
-
-        String newCode = "";
-        String codigo = "";
-        while (!codigos.contains(codigo) && codigo.equals("")) {
-            Random rd = new Random();
-            IntStream code = rd.ints(4, 0, 10);
-            Iterator iterator = code.iterator();
-            while (iterator.hasNext()) {
-                newCode = iterator.next() + "";
-                codigo = codigo + newCode;
-            }
-        }
-        codigos.add(codigo);
-        return codigo;
-    }
-
-    public void confirmarEvento() {
-        this.estado = EstadoEvento.CONFIRMADO;
-    }
-
+    
     public void tipoToString(){
        switch(tipo){
             case BODA:
@@ -176,31 +211,26 @@ public class Evento {
         }
     }    
     
+
     @Override
-    public String toString() {
-        String linea = codigo + "," + cl.getNombre() + ","+ sTipo + "," + hInicio + "," + hFin + "," + capacidad + "," + pl + "," + estado;
+    public String toString(){
+        String linea = codigo+"," + cl.getNombre()+","+sTipo+","+hInicio+","+hFin+","+capacidad+","+pl+","+estado;
         return linea;
     }
-
- 
-    public static void agregarEvento(Evento ev) {
-        eventos.add(ev);
-    }
-
-    public void mostrarMensaje() {
-        System.out.println("f");
-
-    }
+    
     public double calcularCostoAdicionales(){
         
         double costoAdicionales = comida.getPrecioFinal()+bebida.getPrecioFinal()+musica.getPrecioFinal()+fotografia.getPrecioFinal()+bocaditos.getPrecioFinal();
         return costoAdicionales;
     }
+    
     public double calcularCostoTotal(){
         double costoTotal=this.tarifa+this.calcularCostoAdicionales();
         return costoTotal;
     }
+    
+    public static void agregarEvento(Evento ev) {
+        eventos.add(ev);
+    }
 }
     
-   
-}

446e0c49fd3c12dd3e7a2329d613773eb4fb4302
OnionService <74696654+OnionService@users.noreply.github.com>
2020-12-04 21:48:21 -0500

    Update Musica.java

--- src/Eventos/Musica.java
+++ src/Eventos/Musica.java
@@ -6,25 +6,43 @@ import java.util.Scanner;
 public class Musica extends ElementoAdicional {
     private final double precioDj = 300;
     private final double precioBanda = 2000;
-    
+    private String tipo;
+    private String tipo1;
+    private String tipo2;
 
+    
+    public String getTipo(){return tipo;}
+    public String getTipo1(){return tipo1;}
+    public String getTipo2(){return tipo2;}
+    
     @Override
     public void establecerCantidad() {
-        this.setCantidad(1);
         int opcion;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Musica Disponible : \n" + "1.Dj($300)" + "2.Banda($2000)"+"3.Omitir");
-        System.out.println("¿Que prefiere?");
-        opcion =sc.nextInt();
-        switch (opcion){
-            case 1:
-                this.setPrecioFinal(precioDj);
-                break;
-            case 2:
-                this.setPrecioFinal(precioBanda);
-                break;
-            case 3:
-                break;
-        } 
+        do{
+            
+            Scanner sc = new Scanner(System.in);
+            double contadorPrecio=0;
+            System.out.println("Musica Disponible : \n" + "1.Dj($300)" + "2.Banda($2000)"+"3.Omitir");
+            System.out.println("¿Que prefiere?");
+            opcion =sc.nextInt();
+            switch (opcion){
+                case 1:
+                    this.setCantidad(this.getCantidad()+1);
+                    contadorPrecio+=precioDj;
+                    this.tipo="DJ";
+                    this.tipo1="DJ";
+                    break;
+                case 2:
+                    this.setCantidad(this.getCantidad()+1);
+                    contadorPrecio+=precioBanda;
+                    this.tipo="BANDA";
+                    this.tipo2="BANDA";
+                    break;
+                case 3:
+                    break;
+            } 
+            sc.close();
+        }while (opcion!=3&&this.getCantidad()!=2);
+        
     }    
 }
